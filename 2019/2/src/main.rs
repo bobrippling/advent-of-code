a8440a9a3690e8b5abe8c7ed3deb51e2946b2243
Rob Pilling <robpilling@gmail.com>
2019-12-02 17:17:31 +0000

    Day 2 Part 2

--- 2019/2/src/main.rs
+++ 2019/2/src/main.rs
@@ -46,6 +46,25 @@
 //
 // Once you have a working computer, the first step is to restore the gravity assist program (your puzzle input) to the "1202 program alarm" state it had just before the last computer caught fire. To do this, before running the program, replace position 1 with the value 12 and replace position 2 with the value 2. What value is left at position 0 after the program halts?
 
+// part 2
+// "Good, the new computer seems to be working correctly! Keep it nearby during this mission - you'll probably use it again. Real Intcode computers support many more features than your new one, but we'll let you know what they are as you need them."
+//
+// "However, your current priority should be to complete your gravity assist around the Moon. For this mission to succeed, we should settle on some terminology for the parts you've already built."
+//
+// Intcode programs are given as a list of integers; these values are used as the initial state for the computer's memory. When you run an Intcode program, make sure to start by initializing memory to the program's values. A position in memory is called an address (for example, the first value in memory is at "address 0").
+//
+// Opcodes (like 1, 2, or 99) mark the beginning of an instruction. The values used immediately after an opcode, if any, are called the instruction's parameters. For example, in the instruction 1,2,3,4, 1 is the opcode; 2, 3, and 4 are the parameters. The instruction 99 contains only an opcode and has no parameters.
+//
+// The address of the current instruction is called the instruction pointer; it starts at 0. After an instruction finishes, the instruction pointer increases by the number of values in the instruction; until you add more instructions to the computer, this is always 4 (1 opcode + 3 parameters) for the add and multiply instructions. (The halt instruction would increase the instruction pointer by 1, but it halts the program instead.)
+//
+// "With terminology out of the way, we're ready to proceed. To complete the gravity assist, you need to determine what pair of inputs produces the output 19690720."
+//
+// The inputs should still be provided to the program by replacing the values at addresses 1 and 2, just like before. In this program, the value placed in address 1 is called the noun, and the value placed in address 2 is called the verb. Each of the two input values will be between 0 and 99, inclusive.
+//
+// Once the program has halted, its output is available at address 0, also just like before. Each time you try a pair of inputs, make sure you first reset the computer's memory to the values in the program (your puzzle input) - in other words, don't reuse memory from a previous attempt.
+//
+// Find the input noun and verb that cause the program to produce the output 19690720. What is 100 * noun + verb? (For example, if noun=12 and verb=2, the answer would be 1202.)
+
 use std::fs;
 
 type Word = u32;
@@ -67,6 +86,8 @@ fn interpret(bytes: &mut [Word]) {
 
                 bytes[dest as usize] = bytes[src1 as usize]
                     .wrapping_add(bytes[src2 as usize]);
+
+                i += 4;
             },
 
             OP_MUL => {
@@ -75,6 +96,8 @@ fn interpret(bytes: &mut [Word]) {
 
                 bytes[dest as usize] = bytes[src1 as usize]
                     .wrapping_mul(bytes[src2 as usize]);
+
+                i += 4;
             },
 
             OP_HALT => {
@@ -85,8 +108,6 @@ fn interpret(bytes: &mut [Word]) {
                 panic!();
             },
         }
-
-        i += 4;
     }
 }
 
@@ -97,13 +118,11 @@ fn show_bytes(bytes: &[Word]) {
     println!("");
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let s = fs::read_to_string("./input")?;
-    let mut bytes = s
-        .trim_end()
-        .split(",")
-        .map(str::parse)
-        .collect::<Result<Vec<Word>, _>>()?;
+#[allow(dead_code)]
+fn part1(bytes_slice: &[Word]) {
+    let mut bytes = Vec::new();
+    bytes.resize(bytes_slice.len(), 0);
+    bytes.copy_from_slice(bytes_slice);
 
     bytes[1] = 12;
     bytes[2] = 2;
@@ -115,6 +134,53 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("output");
     show_bytes(&bytes);
+}
+
+#[allow(dead_code)]
+fn part2(bytes_slice: &[Word]) {
+    fn find(bytes_slice: &[Word]) -> Option<Word> {
+        let mut bytes = Vec::new();
+        bytes.resize(bytes_slice.len(), 0);
+
+        let desired = 19690720;
+
+        for noun in 0..=99 {
+            for verb in 0..=99 {
+                bytes.copy_from_slice(bytes_slice);
+                bytes[1] = noun;
+                bytes[2] = verb;
+
+                interpret(&mut bytes);
+
+                let output = bytes[0];
+
+                //println!("{} and {} give {}", noun, verb, output);
+
+                if output == desired {
+                    return Some(100 * noun + verb);
+                }
+            }
+        }
+
+        None
+    }
+
+    match find(bytes_slice) {
+        Some(answer) => println!("found: {}", answer),
+        None => println!("Couldn't find match"),
+    };
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let s = fs::read_to_string("./input")?;
+    let bytes = s
+        .trim_end()
+        .split(",")
+        .map(str::parse)
+        .collect::<Result<Vec<Word>, _>>()?;
+
+    //part1(&bytes);
+    part2(&bytes);
 
     Ok(())
 }
