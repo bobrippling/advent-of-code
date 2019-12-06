0d30666179ac818912eef7b2c95fa7ebbb164c16
Rob Pilling <robpilling@gmail.com>
2019-12-06 08:57:52 +0000

    Day 5 Part 2

--- 2019/2/src/main.rs
+++ 2019/2/src/main.rs
@@ -70,6 +70,48 @@
 
 // Opcode 3 takes a single integer as input and saves it to the position given by its only parameter. For example, the instruction 3,50 would take an input value and store it at address 50.
 // Opcode 4 outputs the value of its only parameter. For example, the instruction 4,50 would output the value at address 50.
+//
+//
+//
+// The air conditioner comes online! Its cold air feels good for a while, but then the TEST alarms start to go off. Since the air conditioner can't vent its heat anywhere but back into the spacecraft, it's actually making the air inside the ship warmer.
+
+// Instead, you'll need to use the TEST to extend the thermal radiators. Fortunately, the diagnostic program (your puzzle input) is already equipped for this. Unfortunately, your Intcode computer is not.
+
+// Your computer is only missing a few opcodes:
+
+//     Opcode 5 is jump-if-true: if the first parameter is non-zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+//     Opcode 6 is jump-if-false: if the first parameter is zero, it sets the instruction pointer to the value from the second parameter. Otherwise, it does nothing.
+//     Opcode 7 is less than: if the first parameter is less than the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+//     Opcode 8 is equals: if the first parameter is equal to the second parameter, it stores 1 in the position given by the third parameter. Otherwise, it stores 0.
+
+// Like all instructions, these instructions need to support parameter modes as described above.
+
+// Normally, after an instruction is finished, the instruction pointer increases by the number of values in that instruction. However, if the instruction modifies the instruction pointer, that value is used and the instruction pointer is not automatically increased.
+
+// For example, here are several programs that take one input, compare it to the value 8, and then produce one output:
+
+//     3,9,8,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+//     3,9,7,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+//     3,3,1108,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+//     3,3,1107,-1,8,3,4,3,99 - Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+
+// Here are some jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero:
+
+//     3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9 (using position mode)
+//     3,3,1105,-1,9,1101,0,0,12,4,12,99,1 (using immediate mode)
+
+// Here's a larger example:
+
+// 3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+// 1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+// 999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99
+
+// The above example program uses an input instruction to ask for a single number. The program will then output 999 if the input value is below 8, output 1000 if the input value is equal to 8, or output 1001 if the input value is greater than 8.
+
+// This time, when the TEST diagnostic program runs its input instruction to get the ID of the system to test, provide it 5, the ID for the ship's thermal radiator controller. This diagnostic test suite only outputs one number, the diagnostic code.
+
+// What is the diagnostic code for system ID 5?
+
 
 use std::fs;
 
@@ -167,6 +209,62 @@ fn interpret(bytes: &mut [Word]) {
                 output(if deref { bytes[src as usize] } else { src });
 
                 i += 2;
+            },
+
+            5 => {
+                let (src1, src2) = (bytes[i + 1], bytes[i + 2]);
+                let (deref_1, deref_2) = (decode_param(op, 0), decode_param(op, 1));
+                let (src1_read, src2_read) = (
+                    if deref_1 { bytes[src1 as usize] } else { src1 },
+                    if deref_2 { bytes[src2 as usize] } else { src2 }
+                );
+
+                if src1_read != 0 {
+                    i = src2_read as usize;
+                } else {
+                    i += 3;
+                }
+            },
+
+            6 => {
+                let (src1, src2) = (bytes[i + 1 as usize], bytes[i + 2 as usize]);
+                let (deref_1, deref_2) = (decode_param(op, 0), decode_param(op, 1));
+                let (src1_read, src2_read) = (
+                    if deref_1 { bytes[src1 as usize] } else { src1 },
+                    if deref_2 { bytes[src2 as usize] } else { src2 }
+                );
+
+                if src1_read == 0 {
+                    i = src2_read as usize;
+                } else {
+                    i += 3;
+                }
+            },
+
+            7 => {
+                let (src1, src2, dest) = (bytes[i + 1 as usize], bytes[i + 2 as usize], bytes[i + 3 as usize]);
+                let (deref_1, deref_2) = (decode_param(op, 0), decode_param(op, 1));
+                let (src1_read, src2_read) = (
+                    if deref_1 { bytes[src1 as usize] } else { src1 },
+                    if deref_2 { bytes[src2 as usize] } else { src2 }
+                );
+
+                bytes[dest as usize] = if src1_read < src2_read { 1 } else { 0 };
+
+                i += 4;
+            },
+
+            8 => {
+                let (src1, src2, dest) = (bytes[i + 1 as usize], bytes[i + 2 as usize], bytes[i + 3 as usize]);
+                let (deref_1, deref_2) = (decode_param(op, 0), decode_param(op, 1));
+                let (src1_read, src2_read) = (
+                    if deref_1 { bytes[src1 as usize] } else { src1 },
+                    if deref_2 { bytes[src2 as usize] } else { src2 }
+                );
+
+                bytes[dest as usize] = if src1_read == src2_read { 1 } else { 0 };
+
+                i += 4;
             },
 
             OP_HALT => {
@@ -311,4 +409,14 @@ mod tests {
         interpret(&mut bytes);
         assert_eq!(bytes, [1002,4,3,4,99]);
     }
+
+    /*#[test]
+    fn test_eg8() {
+     3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9
+     3,3,1105,-1,9,1101,0,0,12,4,12,99,1
+    }*/
+//     3,9,8,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+//     3,9,7,9,10,9,4,9,99,-1,8 - Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
+//     3,3,1108,-1,8,3,4,3,99   - Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
+//     3,3,1107,-1,8,3,4,3,99   - Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
 }
