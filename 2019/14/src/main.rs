6d03db665e6b1344fb725594a4a6744a86002cb9
Rob Pilling <robpilling@gmail.com>
2019-12-15 10:40:16 +0000

    Day 14

--- 2019/14/src/main.rs
+++ 2019/14/src/main.rs
@@ -1,4 +1,10 @@
+#![allow(unused_variables)]
 use std::collections::HashMap;
+use std::fs;
+
+const USE_MUL: bool = true;
+const TRILLION: usize = 1000000000000;
+const DEBUG: bool = false;
 
 #[derive(PartialEq, Eq, Hash, Clone)]
 struct Element(String);
@@ -131,16 +137,23 @@ impl Reactions {
     }
     */
 
-    fn iterate(&self, req: &Ingredient) -> HashMap<Element, usize> {
-        let mut tobuild = HashMap::<Element, usize>::new();
-        //let mut roots = HashMap::<Element, usize>::new();
+    fn iterate_internal(
+        &self,
+        tobuild: &mut HashMap<Element, usize>,
+        leftover: &mut HashMap<Element, usize>) {
 
-        tobuild.insert(req.0.clone(), req.1);
+        let mut iterations = 0;
+        loop {
+            iterations += 1;
+            if iterations > 999 {
+                break;
+            }
 
-        while tobuild.len() > 0 {
-            println!("tobuild: {:?}", tobuild);
+                    if DEBUG {
+            println!("tobuild: {:?} (leftover: {:?})", tobuild, leftover);
+                    }
 
-            let mut candidates = tobuild
+            let candidates = tobuild
                 .iter()
                 .filter(|(ref elem, _)| {
                     self.get(&elem).is_some()
@@ -151,11 +164,13 @@ impl Reactions {
                 break;
             }
 
+            /*
             // find the one with the least n
             candidates.sort_by(
                 |a, b| {
                     a.1.cmp(b.1)
                 });
+            */
 
             let first = candidates[0];
 
@@ -163,7 +178,9 @@ impl Reactions {
 
             let (selected, nreq): (Element, usize) = ((*selected).clone(), **nreq);
 
+                    if DEBUG {
             println!("  picked {:?} to build (need {})", selected, nreq);
+                    }
 
             let &(nmade, ref ingredients) = self.get(&selected).unwrap(); /* {
                 Some(x) => x,
@@ -176,32 +193,52 @@ impl Reactions {
                 }
             };*/
 
-            let mut mul = 1;
-            while nmade * mul < nreq {
-                mul += 1;
-            }
+            assert!(nmade > 0);
+            assert!(nreq > 0);
 
+            let mul = if USE_MUL {
+                let mut mul = 1;
+                while nmade * mul < nreq {
+                    mul += 1;
+                }
+                mul
+            } else {
+                1
+            };
+
+
+                    if DEBUG {
             println!("  using recipe {:?} --> {} to make {} {:?} (need {} mul as nreq={})",
                      ingredients, nmade,
                      nmade * mul, selected,
                      mul, nreq);
+                    }
 
-            if nreq < nmade {
+            let nmade = nmade * mul;
+
+            if nmade > nreq {
+                let waste = nmade - nreq;
                 // we've made plenty, remove
                 tobuild.remove(&selected);
+                leftover.insert(selected.clone(), leftover.get(&selected).map(|x| *x).unwrap_or(0) + waste);
+                    if DEBUG {
+                println!("  waste: {}, going in leftover", waste);
+                    }
             } else {
-                let left = nreq - nmade;
-                if left > 0 {
-                    tobuild.insert(selected.clone(), left);
+                let amount_still_tobuild = nreq - nmade;
+                if amount_still_tobuild > 0 {
+                    tobuild.insert(selected.clone(), amount_still_tobuild);
                 } else {
                     tobuild.remove(&selected);
                 }
             }
 
-            for (ref elem, n) in ingredients {
-                /*
-                match tobuild.get(&elem) {
-                    None => {},
+            for (ref elem, mut nreq) in ingredients {
+                // scoped:
+                nreq *= mul;
+
+                let extralog = match leftover.get(&elem) {
+                    None => { false },
                     Some(&already_have) => {
                         let remaining;
                         let to_use;
@@ -214,31 +251,142 @@ impl Reactions {
                             remaining = 0;
                         }
 
-                        println!("  already have {} {:?}s, using {}, leaving {} in tobuild",
+                    if DEBUG {
+                        println!("  already have {} {:?}s, using {}, leaving {} in leftover",
                                  already_have,
-                                 selected,
+                                 elem,
                                  to_use,
                                  remaining);
+                    }
 
-                        tobuild.insert(selected.clone(), remaining);
+                        if remaining > 0 {
+                            leftover.insert(elem.clone(), remaining);
+                        } else {
+                            leftover.remove(&elem);
+                        }
 
                         nreq -= to_use;
+                        true
                     },
                 };
-                */
 
-                let already_tobuild = tobuild.get(elem).map(|x| *x).unwrap_or(0);
+                if extralog || true {
+                    if DEBUG {
+                    println!("  adding {} of {:?} to tobuild", nreq, elem);
+                    }
+                }
 
-                tobuild.insert(elem.clone(), already_tobuild + n /* * mul*/);
+                if nreq == 0 {
+                    continue;
+                }
+
+                let already_tobuild = tobuild.get(elem)
+                    .map(|x| *x)
+                    .unwrap_or(0);
+
+                tobuild.insert(elem.clone(), already_tobuild + nreq);
             }
         }
+    }
+
+    fn iterate(&self, req: &Ingredient,
+               leftover: &mut HashMap<Element, usize>) -> HashMap<Element, usize> {
+        let mut tobuild = HashMap::<Element, usize>::new();
+
+        //let mut roots = HashMap::<Element, usize>::new();
+
+        tobuild.insert(req.0.clone(), req.1);
+
+        self.iterate_internal(&mut tobuild, leftover);
 
         tobuild
     }
+
+    fn iterate2(&self) -> usize {
+        let mut ore = TRILLION;
+        let mut n = 0;
+        let mut leftover = HashMap::<Element, usize>::new();
+        let mut min = 1000000;
+        let mut max = 10000000;
+        let mut guess = min/2+max/2;
+
+        loop {
+            let roots = self.iterate(
+                &(Element::new("FUEL"), guess),
+                &mut leftover);
+
+            assert_eq!(roots.len(), 1);
+
+            let first = roots.iter().nth(0).unwrap();
+            //assert_eq!(first.0, &Element::new("ORE"));
+            let n = *first.1;
+
+            if n > TRILLION {
+                max = guess-1;
+            } else if n < TRILLION {
+                min = guess+1;
+            } else {
+                println!("exact! with {}", guess);
+                break guess;
+            }
+
+            println!("min {} max {} guess of {} fuel requires {} ore",
+                     min,max,guess,n);
+
+            //if min >= max-1 {
+            //    break guess;
+            //}
+
+            guess = max/2 + min/2;
+
+            /*
+            if *first.1 < ore {
+                ore -= *first.1;
+                n += 1;
+            } else {
+                break;
+            }
+            */
+        }
+
+        //n
+    }
 }
 
-fn main() {
-    println!("Hello, world!");
+/*#[allow(dead_code)]
+fn part1(s: &str) {
+    let reactions = Reactions::from(&s);
+    let roots = reactions.iterate(&(Element::new("FUEL"), 1));
+
+    println!("{:?}", roots);
+}*/
+
+fn part2(s: &str) {
+    let reactions = "
+        157 ORE => 5 NZVS
+        165 ORE => 6 DCFZ
+        44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+        12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+        179 ORE => 7 PSHF
+        177 ORE => 5 HKGWZ
+        7 DCFZ, 7 PSHF => 2 XJWVT
+        165 ORE => 2 GPVTF
+        3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT
+            ";
+
+    let reactions = Reactions::from(s);
+
+    let tobuild = reactions.iterate2();
+
+    println!("{}", tobuild);
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let s = fs::read_to_string("./input")?;
+    //part1(&s);
+    part2(&s);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -315,6 +463,7 @@ mod tests {
             assert_eq!(reactions.get(&Element::new("FUEL")), Some(&(1, vec![(Element::new("AB"), 2), (Element::new("BC"), 3), (Element::new("CA"), 4)])));
     }
 
+    /*
     #[test]
     fn eg1() {
         let reactions = "
@@ -419,4 +568,79 @@ mod tests {
         let first = roots.iter().nth(0).unwrap();
         assert_eq!(first, (&Element::new("ORE"), &2210736));
     }
+    */
+
+    #[test]
+    fn part2_eg2() {
+        let reactions = "
+    157 ORE => 5 NZVS
+    165 ORE => 6 DCFZ
+    44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+    12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+    179 ORE => 7 PSHF
+    177 ORE => 5 HKGWZ
+    7 DCFZ, 7 PSHF => 2 XJWVT
+    165 ORE => 2 GPVTF
+    3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT
+        ";
+
+        let reactions = Reactions::from(reactions);
+
+        reactions.iterate2();
+    }
+
+    /*
+    #[test]
+    fn part2_eg3() {
+        let reactions = "
+    2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+    17 NVRVD, 3 JNWZP => 8 VPVL
+    53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+    22 VJHF, 37 MNCFX => 5 FWMGM
+    139 ORE => 4 NVRVD
+    144 ORE => 7 JNWZP
+    5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+    5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+    145 ORE => 6 MNCFX
+    1 NVRVD => 8 CXFTF
+    1 VJHF, 6 MNCFX => 4 RFSQX
+    176 ORE => 6 VJHF
+        ";
+
+        let reactions = Reactions::from(reactions);
+
+        let fuel = reactions.iterate2();
+
+        assert_eq!(fuel, 5586022);
+    }
+
+    #[test]
+    fn part2_eg4() {
+        let reactions = "
+    171 ORE => 8 CNZTR
+    7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+    114 ORE => 4 BHXH
+    14 VRPVC => 6 BMBT
+    6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+    6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+    15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+    13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+    5 BMBT => 4 WPTQ
+    189 ORE => 9 KTJDG
+    1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+    12 VRPVC, 27 CNZTR => 2 XDBXC
+    15 KTJDG, 12 BHXH => 5 XCVML
+    3 BHXH, 2 VRPVC => 7 MZWV
+    121 ORE => 7 VRPVC
+    7 XCVML => 6 RJRHP
+    5 BHXH, 4 VRPVC => 5 LTCX
+        ";
+
+        let reactions = Reactions::from(reactions);
+
+        let fuel = reactions.iterate2();
+
+        assert_eq!(fuel, 460664);
+    }
+    */
 }
